9e929cf059bdbed3cf335dcc1b878599d622325f
Aditya Prakash <aditya.eth@gmail.com>
2021-08-07 17:19:21 +0530

    Array 4 Done

---
+++ Array4.java
@@ -0,0 +1,8 @@
+public class Array4 {
+    public static void main(String[] args) {
+        int[] arr = { 65, 99, 75, 45, 65 };
+        for (int i : arr) {
+            System.out.println((char) i);
+        }
+    }
+}
