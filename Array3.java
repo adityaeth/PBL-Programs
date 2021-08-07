7f275b108bf7edfa9a33c09f96db4250d04b2696
Aditya Prakash <aditya.eth@gmail.com>
2021-08-07 17:14:33 +0530

    array 3 done

--- Array3.java
+++ Array3.java
@@ -1,13 +1,16 @@
 public class Array3 {
     public static void main(String[] args) {
         int[] arr = { 5, 6, 5, 8, 7, 6, 3, 4 };
-        int search = 3;
-        int matches = 0;
-        for (int i : arr) {
-            if (search == i)
-                matches += 1;
+        int search = 5;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (search == arr[i]) {
+                index = i;
+            }
         }
-        if (matches > 0)
-            System.out.println(matches + " Matches Found!");
+        if (index >= 0)
+            System.out.println(index);
+        else
+            System.out.println("-1");
     }
 }
