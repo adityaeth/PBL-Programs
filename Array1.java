184b4fecfb76eedb8b6bbc17b6e9f5706ecc1bed
Aditya Prakash <aditya.eth@gmail.com>
2021-08-07 17:02:34 +0530

    Arrays: 1 2 done

---
+++ Array1.java
@@ -0,0 +1,27 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] num = { 5, 9, 8, 3, 4, 1 };
+        int sum = 0;
+        int max = 0;
+        int min = 10000;
+        for (int i : num) {
+            sum += i;
+        }
+        for (int i : num) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        for (int i : num) {
+            if (min > i) {
+                min = i;
+            }
+        }
+        int avg = sum / num.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Max: " + max);
+        System.out.println("Max: " + max);
+        System.out.println("Avg: " + avg);
+    }
+}
