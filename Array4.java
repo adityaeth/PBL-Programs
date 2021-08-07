8449114dfad4ef3b71c74445da6eb16c0659bd76
Aditya Prakash <aditya.eth@gmail.com>
2021-08-07 18:22:15 +0530

    changes ac to question

--- Array4.java
+++ Array4.java
@@ -2,7 +2,7 @@ public class Array4 {
     public static void main(String[] args) {
         int[] arr = { 65, 99, 75, 45, 65 };
         for (int i : arr) {
-            System.out.println((char) i);
+            System.out.print((char) i+" ");
         }
     }
 }
